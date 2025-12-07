96601b325ad31e5fc201af5611680632e9799de8
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-12-07 21:06:33 +0530

    RecipeSlider file completed as of now

--- src/component/RecipeSlider.jsx
+++ src/component/RecipeSlider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useFetch } from "./useFetch";
 import RecipeCard from "./RecipeCard";
 import Slider from "react-slick";
+import { Clock, Loader } from "lucide-react";
 
 const RecipeSlider = ({ title, fetchUrl }) => {
   const { data, loading, error } = useFetch(fetchUrl);
@@ -18,18 +19,30 @@ const RecipeSlider = ({ title, fetchUrl }) => {
     autoplaySpeed: 2000,
     cssEase: "linear",
   };
+  if (loading)
+    return (
+      <div className="text-center p-8 text-gray-300">
+        <Loader className="animate-spin inline-block mr-2 text-yellow-600" />
+        Loading {title}...
+      </div>
+    );
 
   return (
     <>
       <section>
         <h2 className="text-3xl font-extrabold text-gray-100 mb-6 tracking-tight border-1-4 border-yellow-400 pl-4 flex items-center">
+          <Clock className="w-6 h-6 mr-3 text-blue-600" />
           {title}
         </h2>
-        {meals.map((meal) => (
-          <div key={meal.idMeal} className="px-10 flex justify-center">
-            <RecipeCard meal={meal} />
-          </div>
-        ))}
+        <div style={{ width: "90%", margin: "auto", padding: "10px" }}>
+          <Slider {...settings}>
+            {meals.map((meal) => (
+              <div key={meal.idMeal} className="px-10 flex justify-center">
+                <RecipeCard meal={meal} />
+              </div>
+            ))}
+          </Slider>
+        </div>
       </section>
     </>
   );
