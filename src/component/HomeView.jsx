b48f0c37f79f721b90518538a69e20c8dd31903a
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-12-07 19:52:41 +0530

    working on recipeSlider and some file modified

--- src/component/HomeView.jsx
+++ src/component/HomeView.jsx
@@ -8,14 +8,15 @@ const HomeView = () => {
   return (
     <>
       <main className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-        <TrendingRecipe
-          title="Staff Curated Picks"
-          fetchUrl={`${API_URL}search.php?f=c`}
-        />
         <RecipeSlider
-          title="Quick & Easy Meals"
+          title="Staff Curated Picks "
           fetchUrl={`${API_URL}filter.php?a=Canadian`}
         />
+        <TrendingRecipe
+          title="Quick & Easy Meals"
+          fetchUrl={`${API_URL}search.php?f=c`}
+        />
+
         <CatagorySection />
       </main>
     </>
--- src/component/RecipeSlider.jsx
+++ src/component/RecipeSlider.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import { useFetch } from "./useFetch";
+import RecipeCard from "./RecipeCard";
+import Slider from "react-slick";
 
 const RecipeSlider = ({ title, fetchUrl }) => {
   const { data, loading, error } = useFetch(fetchUrl);
   console.log("Calling meal data = ", data?.meals);
+  const meals = data?.meals || [];
 
-  return <div>RecipeSlider</div>;
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 600,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
+
+  return (
+    <>
+      <section>
+        <h2 className="text-3xl font-extrabold text-gray-100 mb-6 tracking-tight border-1-4 border-yellow-400 pl-4 flex items-center">
+          {title}
+        </h2>
+        {meals.map((meal) => (
+          <div key={meal.idMeal} className="px-10 flex justify-center">
+            <RecipeCard meal={meal} />
+          </div>
+        ))}
+      </section>
+    </>
+  );
 };
 
 export default RecipeSlider;
